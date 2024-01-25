2f11a51d405cc6abd4bc0a9079a32aa8ec91b3d0
huangcheng <huang7899135@126.com>
2024-01-25 14:04:43 +0800

    验证器加个logger 信息

--- vaildator/validator.py
+++ vaildator/validator.py
@@ -77,7 +77,7 @@ class UserNotifyFrequencyValidation(BaseValidation):
                                          UserNotifyFrequency.failure_ticket_id == failure_ticket_id,
                                          FailureTicket.is_done == False)
                                  .first())
-        logger.debug(f"user_notify_frequency:{user_notify_frequency}")
+        logger.warning(f"user_notify_frequency:{user_notify_frequency}")
         if user_notify_frequency:
             next_notify_time = user_notify_frequency.next_notify_time
             current_time = datetime.now()
