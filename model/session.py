cd6d509cbd9615cd6d416e6754f920aabcf2ccf4
huangcheng <huang7899135@126.com>
2024-02-02 15:45:39 +0800

    sql auto commit is no longer supported

--- model/session.py
+++ model/session.py
@@ -5,7 +5,7 @@ from config.setting import DATABASE_URL
 engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=20, pool_recycle=3600, pool_pre_ping=True)
 
 
-SessionLocal = scoped_session(sessionmaker(autocommit=True, autoflush=True, bind=engine))
+SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=True, bind=engine))
 
 
 # engine = create_engine('mysql+pymysql://username:password@localhost/dbname')
